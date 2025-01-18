46820f9fba0ddd7c9cfd01c1521acc68dcd8cb9f
Matt Raffel <matt.raffel@gmail.com>
2025-01-18 05:23:57 -0700

    more blah

---
+++ src/Desktop/README.md
@@ -0,0 +1,4 @@
+
+
+Code  
+https://github.com/Tyrrrz/YoutubeExplode
---
+++ src/Desktop/YouTubeExplodeWrapper.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+namespace SonicStream.Desktop;
+
+/// <summary>
+/// Wraps access to
+///
+/// uses: https://github.com/Tyrrrz/YoutubeExplode
+/// TODO, likely extract this into an interface
+/// </summary>
+public class YouTubeExplodeWrapper(string videoUrl)
+{
+    public async Task<(string, string)> ExtractAudio()
+    {
+        var youtube = new YoutubeClient();
+        var video = await youtube.Videos.GetAsync(videoUrl);
+        
+        var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoUrl);
+        var audioStreamInfo = streamManifest
+            .GetAudioStreams()
+            .Where(s => s.Container == Container.Mp4)
+            .GetWithHighestBitrate();
+            
+        // not perfect as it leaves a file on the system
+        string temporaryAudioFile = $"{Path.Combine(Path.GetTempPath(), Path.GetRandomFileName())}.mp3";
+        if (File.Exists(temporaryAudioFile))
+            File.Delete(temporaryAudioFile);
+            
+        await youtube.Videos.Streams.DownloadAsync(audioStreamInfo, temporaryAudioFile);
+        
+        return (temporaryAudioFile, video.Title);
+    }
+}
